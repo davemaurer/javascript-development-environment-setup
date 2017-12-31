321498fa8c4ac58e0199ce55066fffcf75679118
David Maurer <davemaurermail@gmail.com>
2017-12-30 20:38:00 -0700

    add delete user functionality. create and export function in userAPI.js, use delete function in onclick event in index.js.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import {getUsers} from './api/userApi';
+import {getUsers, deleteUser} from './api/userApi';
 
 // Populate table of users via API call
 getUsers().then(result => {
@@ -16,4 +16,18 @@ getUsers().then(result => {
   });
 
   global.document.getElementById('users').innerHTML = usersBody;
+
+  const deleteLinks = global.document.getElementsByClassName('deleteUser');
+
+  // Must use array.fom to create a real array from a DOM collection. get ElementsByClassname
+  // does not do this, and only returns an array-LIKE object.
+  Array.from(deleteLinks, link => {
+    link.onclick = function(event) {
+      const element = event.target;
+      event.preventDefault();
+      deleteUser(element.attributes["data-id"].value);
+      const row = element.parentNode.parentNode;
+      row.parentNode.removeChild(row)
+    };
+  });
 });
